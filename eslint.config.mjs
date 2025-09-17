30411bad0325bd6e6c7db3054a9ef5772f38e0bd
Praise Jacob <iampraisejacob@gmail.com>
2025-09-18 02:05:34 +0530

    chore: add import sorting to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import tseslint from "typescript-eslint";
 import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
     eslint.configs.recommended,
@@ -15,6 +16,15 @@ export default [
         },
     },
     globalIgnores(["node_modules/", "dist/", "*.config.{js,mjs,ts}"]),
+    {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
+    },
     {
         rules: {
             "@typescript-eslint/no-unused-vars": [
