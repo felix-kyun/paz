f05df2b334e55fe956eebd53f3a640249e43c2f4
Praise Jacob <iampraisejacob@gmail.com>
2025-09-20 21:17:08 +0530

    fix: add error handler for cast error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,24 +1,52 @@
+import { ENV } from "@config";
 import { ServerError } from "@errors/ServerError.error.js";
+import { logger } from "@logger";
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+
+type KnownError = "CastError";
+interface IErrorResponse {
+    code: number;
+    message: string;
+}
+
+const errorMap: Record<KnownError, IErrorResponse> = {
+    CastError: {
+        code: StatusCodes.BAD_REQUEST,
+        message: "Invalid ID format",
+    },
+};
 
 export function errorHandler(
     error: Error,
     _req: Request,
     res: Response,
-    next: NextFunction,
+    _next: NextFunction,
 ): void {
-    if (error instanceof ServerError) {
+    if (isKnownError(error.name)) {
+        const { code, message } = errorMap[error.name];
+        res.status(code).json({
+            error: error.name,
+            message,
+        });
+    } else if (error instanceof ServerError) {
         res.status(error.statusCode).json({
             error: error.name,
             message: error.message,
         });
     } else {
+        logger.error(error, "Unhandled error");
+
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: error.name,
-            message: error.message || "Internal Server Error",
+            message:
+                ENV === "production"
+                    ? "Internal Server Error"
+                    : (error.message ?? "Internal Server Error"),
         });
     }
+}
 
-    next();
+function isKnownError(name: string): name is KnownError {
+    return name in errorMap;
 }
